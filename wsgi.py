e04ef098344824800d6eba9a6c31da27cb9d06be
Ananda Budi Prasetya <ananda.prasetya@ruma.co.id>
2020-07-06 21:03:18 +0700

    Merge the class
    It's a simple flask installation anyway

--- wsgi.py
+++ wsgi.py
@@ -5,5 +5,14 @@ application = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@application.route("/transactions/get_all_transactions", method=["GET"])
+def get_all_transactions():
+    return "Return all transactions"
+
+@application.route("/transactions/get_transaction/<transaction_id>")
+def get_transaction(transaction_id=None)
+	return "Return " + transaction_id
+	
 if __name__ == "__main__":
     application.run()
+
