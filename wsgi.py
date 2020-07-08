5c2f175042b90c68e13a44a35add11bff4a3cb19
Ananda Budi Prasetya <ananda.prasetya@ruma.co.id>
2020-07-08 15:16:00 +0700

    Add new endpoint - API

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from flask_api import FlaskAPI
+
+api = FlaskAPI(__name__)
+
 application = Flask(__name__)
 
 @application.route("/")
@@ -13,6 +17,11 @@ def get_all_transactions():
 def get_transaction(transaction_id=None):
 	return "Return " + transaction_id
 
+@app.route('/example/')
+def example():
+    return {'hello': 'world'}
+
 if __name__ == "__main__":
     application.run()
+    api.run(debug=True)
 
