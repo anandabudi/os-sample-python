2d06cab9708e2064d952516616dbbac6cd124bf2
Ananda Budi Prasetya <ananda.prasetya@ruma.co.id>
2020-07-08 15:19:19 +0700

    Bug Fixing

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,9 @@
-from flask import Flask
+# from flask import Flask
 from flask_api import FlaskAPI
 
-api = FlaskAPI(__name__)
+application = FlaskAPI(__name__)
 
-application = Flask(__name__)
+# application = Flask(__name__)
 
 @application.route("/")
 def hello():
@@ -17,11 +17,11 @@ def get_all_transactions():
 def get_transaction(transaction_id=None):
 	return "Return " + transaction_id
 
-@app.route('/example/')
+@application.route('/example/')
 def example():
     return {'hello': 'world'}
 
 if __name__ == "__main__":
-    application.run()
-    api.run(debug=True)
+    # application.run()
+    application.run(debug=True)
 
