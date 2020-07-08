7b06763949b3cd996520c27d8a8892469d1654b5
Ananda Budi Prasetya <ananda.prasetya@ruma.co.id>
2020-07-08 16:26:22 +0700

    Added method

--- wsgi.py
+++ wsgi.py
@@ -5,19 +5,19 @@ application = FlaskAPI(__name__)
 
 # application = Flask(__name__)
 
-@application.route("/")
+@application.route("/", methods=['GET'])
 def hello():
     return "Hello World!"
 
-@application.route("/transactions/get_all_transactions")
+@application.route("/transactions/get_all_transactions", methods=['GET'])
 def get_all_transactions():
     return "Return all transactions"
 
-@application.route("/transactions/get_transaction/<transaction_id>")
+@application.route("/transactions/get_transaction/<transaction_id>", methods=['GET', 'POST'])
 def get_transaction(transaction_id=None):
 	return "Return " + transaction_id
 
-@application.route('/example/')
+@application.route('/example', methods=['GET'])
 def example():
     return {'hello': 'world'}
 
